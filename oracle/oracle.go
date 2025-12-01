6a59993a3a19b838866a19f59b7e931a1949ed6e
jawad-arb123 <jawad.arbahi12@gmail.com>
2025-12-01 17:41:29 +0100

    server version 19

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -308,6 +308,7 @@ func (d Dialector) RollbackTo(tx *gorm.DB, name string) error {
 
 // GetServerVersion retrieves the Oracle server version as an integer.
 func GetServerVersion(db *gorm.DB) (int, error) {
+	return 19, nil
 	sqlDB, err := db.DB()
 	if err != nil {
 		return 0, err
